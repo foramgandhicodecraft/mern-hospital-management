72485328a23902cff5c3d17cb3f29247b93f50ac
Foram Gandhi <foramgandhi2009@gmail.com>
2025-07-12 17:37:21 +0530

    frontend commit

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -12,11 +12,13 @@ const Contact = () => {
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
-          <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: greatstackdev@gmail.com</p>
-          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
-          <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
+          <p>OUR OFFICE</p>
+          <p>Silver Coin, first floor<br />
+           Vadodara, Gujarat</p>
+          <p>Tel: 9824088605<br />
+           Email: aatmiyahospital@gmail.com</p>
+          <p>CARRERS AT PRESCRIPTO</p>
+          <p>learn more about our teams and job openings</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
       </div>
